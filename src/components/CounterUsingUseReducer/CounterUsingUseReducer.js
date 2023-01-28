87f0af77a6ec7d8b5d61b34cb93e2c2067e9dbdc
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-01-28 23:10:11 +0600

    Counter using use Reducer Added

--- src/components/CounterUsingUseReducer/CounterUsingUseReducer.js
+++ src/components/CounterUsingUseReducer/CounterUsingUseReducer.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './CounterUsingUseReducer.module.css';
 
 const CounterUsingUseReducer = () => {
+
+    const initialState = 0;
+
+    const reducer = (state, action) => {
+        if (action.type === "INCREMENT") {
+            return state + action.payload.count;
+        } else if (action.type === "DECREMENT") {
+            return state - action.payload.count;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     return (
         <div>
             <h2>Counter Using Use Reducer</h2>
             <hr />
-            <h1>Count: </h1>
-            <button>+</button>
-            <button>-</button>
+            <h1>Count: {state}</h1>
+            <button onClick={() => dispatch({ type: "INCREMENT", payload: { count: 5 } })}>+</button>
+            <button onClick={() => dispatch({ type: "DECREMENT", payload: { count: 5 } })}>-</button>
         </div>
     );
 };
